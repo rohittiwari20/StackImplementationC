3b74183df3249acc7b9f89458cf32887a80a118b
rohittiwari20 <63039834+rohittiwari20@users.noreply.github.com>
2020-09-26 20:38:40 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define Max 10
+int count=0;
+struct stack
+{
+int top;
+int items[Max];
+};
+/*To push elements in the stack */
+void push(struct stack *s,int newitem)
+{
+    if(s->top==Max-1)
+    {
+        printf("Stack is full.");
+    }
+    else
+    {
+        s->top++;
+        s->items[s->top]=newitem;
+    }
+    count++;
+}
+void pop(struct stack *s)
+{
+    if(s->top==-1)
+    {
+        printf("Stack is Empty.");
+    }
+    else
+    {
+        printf("Item popped=%d",s->items[s->top]);
+        s->top--;
+    }
+    printf("\n");
+    count--;
+}
+void printStack(struct stack *s)
+{
+    for(int i=0;i<count;i++)
+    {
+        printf("%d->",s->items[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    struct stack *s;
+    s=(struct stack*)malloc(sizeof(struct stack));
+    //creating empty stack
+    s->top=-1;
+    push(s,1);
+    push(s,2);
+    printStack(s);
+    pop(s);
+    printStack(s);
+    return 0;
+}
